f1db55b9730c88f15a417796cc4758735805366a
Sivaprasad Chidambaram <csivaprasad.c@icloud.com>
2023-05-09 22:22:24 +0530

    Update trivy-k8s-scan.sh

--- trivy-k8s-scan.sh
+++ trivy-k8s-scan.sh
@@ -2,8 +2,8 @@
 
 echo $imageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity LOW,MEDIUM,HIGH --light $imageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $imageName
 
 exit_code=$?
 echo "Exit code : ${exit_code}"
