d5b79d524f762825135396da383d9a3427444932
Sivaprasad Chidambaram <csivaprasad.c@icloud.com>
2023-05-14 14:16:10 +0530

    Slack Advanced

--- vars/SendNotification.groovy
+++ vars/SendNotification.groovy
@@ -2,16 +2,111 @@ def call(String buildStatus = 'STARTED') {
     buildStatus = buildStatus ?: 'SUCCESS'
 
     def color
+    def emoji
 
     if(buildStatus == 'SUCCESS') {
         color = '#47ec05'
+        emoji = ':ww:'
     } else if(buildStatus == 'UNSTABLE') {
         color = '#d5ee0d'
+        emoji = ':deadpool:'
     } else {
         color = '#ec2805'
+        emoji = ':hulk:'
     }
 
-    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+    attachments = [
+        [
+            "color": color,
+            "blocks": [
+                [
+                    "type": "header",
+                    "text": [
+                        "type": "plain_text",
+                        "text": "K8S Deployment - ${deploymentName} Pipeline ${env.emoji}",
+                        "emoji": true
+                    ]
+                ],
+                [
+                    "type": "section",
+                    "fields": [
+                        [
+                            "type": "mrkdwn",
+                            "text": "*Job Name:*\n${env.JOB_NAME}"
+                        ],
+                        [
+                            "type": "mrkdwn",
+                            "text": "*Build Number:*\n${env.BUILD_NUMBER}"
+                        ]
+                    ],
+                    "accessory": [
+                        "type": "image",
+                        "image_url": "https://raw.githubusercontent.com/sidd-harth/numeric/main/images/jenkins-slacks.png",
+                        "alt_text": "Slack Icon"
+                    ] 
+                ],
+                [
+                    "type": "section",
+                    "text": [
+                        "type": "mrkdwn",
+                        "text": "*Failed Stage Name:* `${env.failedStage}`"
+                    ],
+                    "accessory": [
+                        "type": "button",
+                        "text": [
+                            "type": "plain_text",
+                            "text": "Jenkins Build URL",
+                            "emoji": true                          
+                        ],
+                        "value": "click_me_123",
+                        "url": "${env.BUILD_URL}",
+                        "action_id": "button-action"
+                    ]
+                ],
+                [
+                    "type": "divider"
+                ],
+                [
+                    "type": "section",
+                    "fields": [
+                        [
+                            "type": "mrkdwn",
+                            "text": "*Git Commit:*\n${GIT_COMMIT}"
+                        ],
+                        [
+                            "type": "mrkdwn",
+                            "text": "*Git Previous Success Commit:*\n${GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
+                        ],
+                    ],
+                    "accessory": [
+                        "type": "image",
+                        "image_url": "https://raw.githubusercontent.com/sidd-harth/numeric/main/images/github-slack.png",
+                        "alt_text": "GitHub Icon"                      
+                    ]
+                ],
+                [
+                    "type": "section",
+                    "text": [
+                        "type": "mrkdwn",
+                        "text": "*Git Branch: * `${GIT_BRANCH}`"
+                    ],
+                    "accessory": [
+                        "type": "button",
+                        "text": [
+                            "type": "plain_text",
+                            "text": "Github Repo URL",
+                            "emoji": true                          
+                        ],
+                        "value": "click_me_123",
+                        "url": "${env.GIT_URL}",
+                        "action_id": "button-action"
+                    ]
+                ]
+            ]
+        ]
+    ]
 
-    slackSend(color: color, message: msg)
+    // def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+    slackSend(iconEmoji: emoji, attachments: attachments)
 }
